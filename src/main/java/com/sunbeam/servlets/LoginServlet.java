c2c693ff0d2120166594631deddb459a22082c7a
Vaibhav Shinde <vaibhavshinde1401@gmail.com>
2025-06-04 17:27:46 +0530

    Added index.html and LoginServlet

---
+++ src/main/java/com/sunbeam/servlets/LoginServlet.java
@@ -0,0 +1,56 @@
+package com.sunbeam.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.sunbeam.daos.UserDao;
+import com.sunbeam.daos.UserDaoImpl;
+import com.sunbeam.entities.User;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		doPost(req, resp);	
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String email = req.getParameter("email");
+		String passwd = req.getParameter("passwd");
+		try(UserDao userDao = new UserDaoImpl()){
+			User dbUser = userDao.findByEmail(email);
+			if(dbUser!=null && dbUser.getPassword().equals(passwd)) {
+				
+			}else {
+				resp.setContentType("text/html");
+				PrintWriter out = resp.getWriter();
+				out.println("<html>");
+				out.println("<head>");
+				out.println("<title>Failed</title>");
+				out.println("</head>");
+				out.println("<body>");
+				out.println("Invalid email or password.");
+				out.println("<br/>");
+				out.println("<a href='index.html'>Login again</a>");
+				out.println("</body>");
+				out.println("</html>");
+			}
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+	}
+	
+}
